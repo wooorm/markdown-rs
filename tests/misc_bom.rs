1cd34ef54f7a62998eb87c41cd794d3a426d6324
Titus Wormer <tituswormer@gmail.com>
2022-06-15 13:08:21 +0200

    Add tests for boms

---
+++ tests/misc_bom.rs
@@ -0,0 +1,15 @@
+extern crate micromark;
+// use micromark::micromark;
+
+#[test]
+fn bom() {
+    // // To do: BOM.
+    // assert_eq!(micromark("\u{FEFF}"), "", "should ignore just a bom");
+
+    // // To do: BOM.
+    // assert_eq!(
+    //     micromark("\u{FEFF}# hea\u{FEFF}ding"),
+    //     "<h1>hea\u{FEFF}ding</h1>",
+    //     "should ignore a bom"
+    // );
+}
