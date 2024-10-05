5faeb16286ceaafad2f5abed61246a7d7aac7949
Bnchi <bnchifahmi@gmail.com>
2024-10-05 09:44:14 +0300

    Add comments

--- mdast_util_to_markdown/src/configure.rs
+++ mdast_util_to_markdown/src/configure.rs
@@ -1,27 +1,56 @@
 pub struct Options {
+    /// Marker to use for bullets of items in unordered lists ('*', '+', or '-', default: '*').
     pub bullet: char,
+    // Marker to use in certain cases where the primary bullet doesn’t work
+    // ('*', '+', or '-', default: '-' when bullet is '*', '*' otherwise).
     pub bullet_other: char,
+    /// Marker to use for bullets of items in ordered lists ('.' or ')', default: '.').
     pub bullet_ordered: char,
+    /// Marker to use for emphasis ('*' or '_', default: '*').
     pub emphasis: char,
+    // Marker to use for fenced code ('`' or '~', default: '`').
     pub fence: char,
+    /// Whether to use fenced code always (boolean, default: true).
+    /// The default is to use fenced code if there is a language defined,
+    /// if the code is empty, or if it starts or ends in blank lines.
     pub fences: bool,
+    // How to indent the content of list items (default: 'IndentOptions::One').
     pub list_item_indent: IndentOptions,
+    /// Marker to use for titles ('"' or "'", default: '"').
     pub quote: char,
+    /// Marker to use for thematic breaks ('*', '-', or '_', default: '*').
     pub rule: char,
+    // Marker to use for strong ('*' or '_', default: '*').
     pub strong: char,
+    // Whether to increment the counter of ordered lists items (bool, default: true).
     pub increment_list_marker: bool,
+    /// Whether to add the same number of number signs (#) at the end of an ATX heading as the
+    /// opening sequence (bool, default: false).
     pub close_atx: bool,
+    /// Whether to always use resource links (boolean, default: false). The default is to use
+    /// autolinks (<https://example.com>) when possible and resource links ([text](url)) otherwise.
     pub resource_link: bool,
+    /// Whether to add spaces between markers in thematic breaks (bool, default: false).
     pub rule_spaces: bool,
+    /// Whether to use setext headings when possible (bool, default: false).
+    /// The default is to always use ATX headings (# heading) instead of
+    /// setext headings (heading\n=======).
+    /// Setext headings cannot be used for empty headings or headings with a rank of three or more.
     pub setext: bool,
+    /// Whether to join definitions without a blank line (bool, default: false).
     pub tight_definitions: bool,
+    // Number of markers to use for thematic breaks (u32, default: 3, min: 3).
     pub rule_repetition: u32,
 }
 
 #[derive(Copy, Clone)]
 pub enum IndentOptions {
+    // Depends on the item and its parent list uses 'One' if the item and list are tight
+    // and 'Tab' otherwise.
     Mixed,
+    // The size of the bullet plus one space.
     One,
+    /// Tab stop.
     Tab,
 }
 
