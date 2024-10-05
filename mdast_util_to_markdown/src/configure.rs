c73a56b9d89d64af744660512aab010b29116c93
Bnchi <bnchifahmi@gmail.com>
2024-10-05 10:10:01 +0300

    Update comments wrapping

--- mdast_util_to_markdown/src/configure.rs
+++ mdast_util_to_markdown/src/configure.rs
@@ -10,9 +10,8 @@ pub struct Options {
     pub emphasis: char,
     // Marker to use for fenced code ('`' or '~', default: '`').
     pub fence: char,
-    /// Whether to use fenced code always (bool, default: true).
-    /// The default is to use fenced code if there is a language defined,
-    /// if the code is empty, or if it starts or ends in blank lines.
+    /// Whether to use fenced code always (bool, default: true). The default is to use fenced code
+    /// if there is a language defined, if the code is empty, or if it starts or ends in blank lines.
     pub fences: bool,
     // How to indent the content of list items (default: 'IndentOptions::One').
     pub list_item_indent: IndentOptions,
@@ -27,15 +26,14 @@ pub struct Options {
     /// Whether to add the same number of number signs (#) at the end of an ATX heading as the
     /// opening sequence (bool, default: false).
     pub close_atx: bool,
-    /// Whether to always use resource links (bool, default: false). The default is to use
-    /// autolinks (<https://example.com>) when possible and resource links ([text](url)) otherwise.
+    /// Whether to always use resource links (bool, default: false). The default is to use autolinks
+    /// (<https://example.com>) when possible and resource links ([text](url)) otherwise.
     pub resource_link: bool,
     /// Whether to add spaces between markers in thematic breaks (bool, default: false).
     pub rule_spaces: bool,
-    /// Whether to use setext headings when possible (bool, default: false).
-    /// The default is to always use ATX headings (# heading) instead of
-    /// setext headings (heading\n=======).
-    /// Setext headings cannot be used for empty headings or headings with a rank of three or more.
+    /// Whether to use setext headings when possible (bool, default: false). The default is to always
+    /// use ATX headings (# heading) instead of setext headings (heading\n=======). Setext headings
+    /// cannot be used for empty headings or headings with a rank of three or more.
     pub setext: bool,
     /// Whether to join definitions without a blank line (bool, default: false).
     pub tight_definitions: bool,
@@ -45,8 +43,8 @@ pub struct Options {
 
 #[derive(Copy, Clone)]
 pub enum IndentOptions {
-    // Depends on the item and its parent list uses 'One' if the item and list are tight
-    // and 'Tab' otherwise.
+    // Depends on the item and its parent list uses 'One' if the item and list are tight and 'Tab'
+    // otherwise.
     Mixed,
     // The size of the bullet plus one space.
     One,
