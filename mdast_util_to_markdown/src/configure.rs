7e286efd5adb49eb88e68a2341ad018ce3bc4e15
Bnchi <bnchifahmi@gmail.com>
2024-10-11 16:12:53 +0300

    Update docs

--- mdast_util_to_markdown/src/configure.rs
+++ mdast_util_to_markdown/src/configure.rs
@@ -65,9 +65,10 @@ pub struct Options {
     /// Setext headings cannot be used for empty headings or headings with a
     /// rank of three or more.
     pub setext: bool,
-    /// (`bool`, default: `true`) — whether to support math (text) with a single dollar.
+    /// Whether to support math (text) with a single dollar (`bool`, default: `true`).
     /// Single dollars work in Pandoc and many other places, but often interfere with “normal”
-    /// dollars in text. If you turn this off, you can still use two or more dollars for text math
+    /// dollars in text.
+    /// If you turn this off, you can still use two or more dollars for text math.
     pub single_dollar_text_math: bool,
     /// Marker to use for strong (`'*'` or `'_'`, default: `'*'`).
     pub strong: char,
